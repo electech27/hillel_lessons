93cde7c69578603f72402eea6e91e051c0f15fb4
Hello_world <ivanlupashku09@gmail.com>
2025-02-13 16:06:03 +0200

    Created first file 'Hello world'

---
+++ Python_lesson2.py
@@ -0,0 +1,12 @@
+input_user = '1234'
+number = int(input_user)
+ca1 = number // 1000
+ca2 = (number % 1000) // 100
+ca3 = (number % 100) // 10
+ca4 = number % 10
+print(ca1)
+print(ca2)
+print(ca3)
+print(ca4)
+
+
