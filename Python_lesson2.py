a68035ef81049f946e9c3b1f11ebe28609a99713
Hello_world <ivanlupashku09@gmail.com>
2025-02-14 12:15:33 +0200

    Created first file 'Hello world'

--- Python_lesson2.py
+++ Python_lesson2.py
@@ -9,4 +9,11 @@ print(ca2)
 print(ca3)
 print(ca4)
 
-
+input_num = '12345'
+ab = int(input_num)
+a1 = ab % 10
+a2 =  (ab // 10) % 10
+a3 = (ab // 100) % 10
+a4 = (ab // 1000) % 10
+a5 = (ab // 10000) % 10
+print(a1, a2, a3, a4, a5)
