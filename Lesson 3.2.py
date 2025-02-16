4bb0cc9dc680a4a17ae0c531d733deaab25a832d
Hello_world <ivanlupashku09@gmail.com>
2025-02-16 23:55:18 +0200

    Created first file 'Hello world'

---
+++ Lesson 3.2.py
@@ -0,0 +1,17 @@
+list_one = [5,6,7,8]
+list_one.insert(0, list_one.pop())
+print(list_one)
+
+list_two = [1]
+list_two.insert(0, list_two.pop())
+print(list_two)
+
+list_three = []
+if list_three:
+    list_three.insert(0, list_three.pop())
+print(list_three)
+
+list_one = [10,11,12,13,14]
+list_one.insert(0, list_one.pop())
+print(list_one)
+
