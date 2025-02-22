f118e50ca6f1291e176d5e9d0c391022ae570c58
Hello_world <ivanlupashku09@gmail.com>
2025-02-22 19:36:37 +0200

    Created first file 'Hello world'

--- Lesson4.1.py
+++ Lesson4.1.py
@@ -1,14 +1,11 @@
-# [0, 1, 0, 12, 3] -> [1, 12, 3, 0, 0]
-# [0] -> [0]
-#[1, 0, 13, 0, 0, 0, 5] -> [1, 13, 5, 0, 0, 0, 0]
-#[9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] -> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
-
-
 first_list = [0,1,0,12,3]
 print(first_list[1:2] + first_list[3:] + first_list[:1] + first_list[2:3])
 
 second_list = [0]
-print(second_list[1:0] + second_list)
+
+
+print(second_list[1:0] + second_list) #Здесь будет лишним second_list[1:0] так как просто можно оставить print(second_list)
+
 
 
 third_list = [1, 0, 13, 0, 0, 0, 5]
