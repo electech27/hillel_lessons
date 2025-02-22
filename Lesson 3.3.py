ebd385b6ba7046315b0b841db08f198aa340da26
Hello_world <ivanlupashku09@gmail.com>
2025-02-22 15:08:07 +0200

    Created first file 'Hello world'

---
+++ Lesson 3.3.py
@@ -0,0 +1,25 @@
+list_one = [1,2,3,4,5,6]
+x_1 = list_one[:3]
+y_1 = list_one[3:]
+print([x_1, y_1])
+
+list_two = [1,2,3]
+x_2 = list_two[:2]
+y_2 = list_two[2:]
+print([x_2, y_2])
+
+list_three = [1,2,3,4,5]
+x_3 = list_three[:3]
+y_3 = list_three[3:]
+print([x_3, y_3])
+
+list_four = [1]
+x_4 = list_four[:1]
+y_4 = list_four[:0]
+print([x_4, y_4])
+
+list_five = []
+x_5 = list_five[:0]
+y_5 = list_five[0:]
+print([x_5, y_5])
+
