e8f662bd79e727cb5a75d893f55bf817e48bb605
Hello_world <ivanlupashku09@gmail.com>
2025-02-22 19:26:15 +0200

    Created first file 'Hello world'

---
+++ Lesson4.1.py
@@ -0,0 +1,20 @@
+# [0, 1, 0, 12, 3] -> [1, 12, 3, 0, 0]
+# [0] -> [0]
+#[1, 0, 13, 0, 0, 0, 5] -> [1, 13, 5, 0, 0, 0, 0]
+#[9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] -> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
+
+
+first_list = [0,1,0,12,3]
+print(first_list[1:2] + first_list[3:] + first_list[:1] + first_list[2:3])
+
+second_list = [0]
+print(second_list[1:0] + second_list)
+
+
+third_list = [1, 0, 13, 0, 0, 0, 5]
+print(third_list[0:3:2] + third_list[6:] + third_list[1:2] + third_list[3:6:1])
+
+four_list = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+four_1 = four_list[0:3:2] + four_list[3:10:2] + four_list[12:13]
+four_2 = four_list[1:5:3] + four_list[6:11:2] + four_list[11::2]
+print(four_1 + four_2)
